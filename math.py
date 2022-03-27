ebe632d4fe652efb83a011d459ef4da4300e6e0d
Ron Schmitt <ron.schmit@caa.columbia.edu>
2022-03-26 22:14:21 -0400

    sectin 2

---
+++ math.py
@@ -0,0 +1,15 @@
+import math  # needed for Pi
+
+5**3  # ** is exponential like in Fortran
+5 // 3 # integer division 
+5 % 3   # remainder
+round(math.pi,2)  
+
+int(math.pi)
+# _ gives most recent result 
+
+2*_
+
+int(pi)
+float(8)
+

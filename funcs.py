5255c982eb0c423c283f536fc39de8a1a9a9682a
Ron Schmitt <ron.schmit@caa.columbia.edu>
2022-03-28 15:14:08 -0400

    section 7

---
+++ funcs.py
@@ -0,0 +1,36 @@
+
+
+def hello():
+    print("Hello World")
+
+
+hello()
+
+def add(x1,x2):
+    print(x1+x2)
+
+add(3,4)    
+
+def sub(x1,x2):
+    print(x1-x2)
+
+sub(4,3)    
+sub(x2=4,x1=3)
+
+def pname(name="nobody"):
+    print(name)
+
+pname("Ron")
+pname()
+
+def addtwo(num1, num2):
+    return num1 + num2
+
+print( addtwo(3,6) )
+
+
+
+def hey():
+    print("hey")
+
+print( hey()   )
